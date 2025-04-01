b5194d175010ed0eb988d6f6b95961e8458ac8e7
IcySpar <victorasielrodriguez@gmail.com>
2025-04-01 16:49:01 -0700

    feat: add simple validation chain to sign up form
    Errors are presented on form page for user to view

--- controllers/formControllers.js
+++ controllers/formControllers.js
@@ -2,16 +2,56 @@ require('dotenv').config();
 
 const { genPassword } = require('../lib/passwordUtils');
 const db = require('../db/queries');
+const { body, validationResult } = require("express-validator");
+
+
+
+const validateUser = [
+  body('username').trim()
+    .isAlphanumeric().withMessage('Username must be an alphanumeric value.')
+    .isLength({min: 1, max: 25}).withMessage('Username must be between 1-25 characters.'),
+  body('password').trim()
+    .exists({checkFalsy: true}).withMessage('You must type a passwrd.'),
+  body('confirmPassword').trim()
+    .exists({checkFalsy: true}).withMessage('You must type a confirmation password')
+    .custom((value, {req}) => value === req.body.password).withMessage('The Passwords do not match.'),
+  body('firstName').trim()
+    .exists({checkFalsy: true}).withMessage('You must complete the first name field.')
+    .isAlpha().withMessage('First Name must only contain letters.'),
+  body('lastName').trim()
+    .exists({checkFalsy: true}).withMessage('You must complete the last name field.')
+    .isAlpha().withMessage('Last Name must only contain letters.')
+];
+
+
+
+
+
+
+
+
 
 const getSignUpFormController = (req, res) => {
-    res.render('sign-up-form');
+    res.render('sign-up-form', {isLoggedIn: req.isAuthenticated()});
 }
 
-const postSignUpFormController = async (req, res) => {
+const postSignUpFormController = [
+    validateUser,
+async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).render('sign-up-form', {
+            errors: errors.array(),
+            isLoggedIn: req.isAuthenticated(),
+        });
+    }
+
+
     const hashAndSalt = await genPassword(req.body.password);
     await db.postUserInfo(req.body.username, hashAndSalt.hash, hashAndSalt.salt, req.body.firstName, req.body.lastName);
     res.redirect('/form/sign-up');
-}
+}];
 
 const getLogInFormController = (req, res) => {
     res.render('log-in-form', {title: 'Log In'});
@@ -36,7 +76,7 @@ const postMemberSignUpForm = async (req, res) => {
         res.redirect('/form/member-sign-up');
     }
 
-}
+};
 
 module.exports = {
     getSignUpFormController,
